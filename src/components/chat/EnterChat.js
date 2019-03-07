35b49d2805ab557a5173fc217f0a949b6d21be02
JP S <github@shipherd.com>
2019-03-07 00:55:03 -0500

    Change to a dropdown list of canned customers

--- src/components/chat/EnterChat.js
+++ src/components/chat/EnterChat.js
@@ -1,50 +1,38 @@
 import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
 
 class EnterChat extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      username: ''
+      username: '',
+      possibleUsers: require('./users.json')
     }
   }
 
-  changeUsername(e) {
-    e.preventDefault();
-    if (this.state.username.length) {
-      this.props.setUsername(this.state.username);
+  changeUser(i) {
+    if (this.state.possibleUsers[i]) {
+      this.props.setUserKey({
+        token: this.state.possibleUsers[i].token,
+        roomId: this.state.possibleUsers[i].rooms[0]});
     } else {
       alert('Please provide a username');
     }
   }
 
-  onChange(e) {
-    this.setState({
-      username: e.target.value
-    })
-  }
-
   render() {
     return (
       <div className="enter-chat d-flex justify-content-center align-items-center">
-        <form className="col-xs-12 col-sm-12 col-md-6 col-lg-4" onSubmit={this.changeUsername.bind(this)}>
-          <React.Fragment>
-            <div className="input-group ">
-              <input
-                className="form-control"
-                placeholder="Username"
-                value={this.state.username}
-                onChange={this.onChange.bind(this)}
-              />
-              <div className="input-group-append">
-                <button
-                  className="btn btn-outline-secondary"
-                  type="submit">
-                  Join
-                </button>
-              </div>
-            </div>
-          </React.Fragment>
+        <form className="col-xs-12 col-sm-12 col-md-6 col-lg-4" >
+          <DropdownButton title='Select User' onSelect={this.changeUser.bind(this)}>
+            {this.state.possibleUsers.map((user, i) => {
+              return (
+                <Dropdown.Item key={i} eventKey={i}>{user.name}</Dropdown.Item>
+              )
+            })}
+          </DropdownButton>
         </form>
       </div>
     )
