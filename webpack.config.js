7254a9b221329572bedf7b0141ed33c60ba8844a
JP S <github@shipherd.com>
2019-03-07 00:53:31 -0500

    add fs,net,tls "empty" node config.  Needed after I started using request

--- webpack.config.js
+++ webpack.config.js
@@ -76,4 +76,9 @@ module.exports = {
   performance: {
     hints: process.env.NODE_ENV === 'production' ? "warning" : false
   },
+  node: {
+    fs: 'empty', 
+    net: 'empty',
+    tls: 'empty'
+  }
 };
